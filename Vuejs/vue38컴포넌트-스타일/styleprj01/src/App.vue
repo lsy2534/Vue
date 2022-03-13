1e0bd0799712db441d7fb3ebc2b45688d623453a
kkks <lsy4444@gmail.com>
2022-03-13 11:54:10 +0900

    22-03-13

---
+++ Vuejs/vue38컴포넌트-스타일/styleprj01/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <h2>{{ msg }}</h2>
+        <child1 />
+        <child2 />
+    </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue';
+import Child1 from './components/Comp_Child1.vue';
+import Child2 from './components/Comp_Child2.vue';
+
+export default {
+    props: [],
+    data: function () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        };
+    },
+    components: {
+        child1: Child1,
+        child2: Child2
+    }
+};
+</script>
+
+<style>
+</style>
